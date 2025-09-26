6c7748e6238da924119bb431bc2e0f3419a702bc
Vu Tuan Thanh <thanhvt.mbb@gmail.com>
2025-09-26 23:48:17 +0700

    Trang chu ver 1

--- HikariNoMichi/src/app/app.routes.ts
+++ HikariNoMichi/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+
 
 export const routes: Routes = [];
+
+    //{ path: '', component: Home, title: 'Home page', },
+    //{ path: 'details/:id', component: Details, title: 'Home details', },];
--- HikariNoMichi/src/app/app.ts
+++ HikariNoMichi/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Home } from './home/home';
+import { HomeFooter } from './home-footer/home-footer';
+import { HomeBanner } from './home-banner/home-banner';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Home, HomeBanner, HomeFooter, RouterModule],
+  template: `<app-home-banner></app-home-banner>
+            <app-home></app-home>
+            <app-home-footer></app-home-footer>`,
+  styleUrls: ['./app.css'],
 })
+
+
 export class App {
-  protected readonly title = signal('HikariNoMichi');
+  title = 'Trang chủ';
 }
